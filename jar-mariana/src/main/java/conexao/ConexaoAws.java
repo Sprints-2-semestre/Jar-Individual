7f34bf37a90526293d20e1f350583a93f27d2ec3
MarianNdO <mariana.oliveira@sptech.school>
2023-11-26 22:29:13 -0300

    Testando o funcionamento do individual

---
+++ jar-mariana/src/main/java/conexao/ConexaoAws.java
@@ -0,0 +1,24 @@
+package conexao;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class ConexaoAws {
+
+    private JdbcTemplate conexaoDoBanco;
+    public ConexaoAws() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        dataSource.setUrl("jdbc:sqlserver://54.197.240.46:1433;database=farmacos;"
+                + "user=sa;password=farmacos;encrypt=true;"
+                + "trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("farmacos");
+        this.conexaoDoBanco = new JdbcTemplate(dataSource);
+    }
+    public JdbcTemplate getConexaoDoBanco() {
+        return conexaoDoBanco;
+    }
+}
