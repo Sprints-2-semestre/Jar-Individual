8f8b635127403c804140a82e1136f84dd7e17588
Quintans <vitor.isilva@sptech.school>
2023-11-27 15:45:23 -0300

    Jar Quintans

---
+++ individual-quintans/src/main/java/conexao/ConexaoSlack.java
@@ -0,0 +1,26 @@
+package conexao;
+
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.Payload;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConexaoSlack {
+    private static HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
---
+++ individual-quintans/src/main/java/teste/Teste.java
@@ -0,0 +1,125 @@
+package teste;
+
+import conexao.Conexao;
+import dao.DadosComponentesDao;
+import dao.MaquinaDao;
+import dao.MaquinaTipoComponenteDao;
+import dao.ValidacaoEmail;
+import modelo.Maquina;
+import modelo.MaquinaTipoComponente;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Teste {
+    public static void main(String[] args) throws IOException {
+        ValidacaoEmail validacaoEmail = new ValidacaoEmail();
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println(
+                """
+                JAR QUINTANS""");
+
+        System.out.println("""
+                ------------------------------------------------
+                |           Seja Bem Vindo a Fármacos          |
+                ------------------------------------------------
+                | Digite o seu email para verificarmos se você |
+                | tem acesso ao nosso sistema                  |
+                |                                              |
+                | Digite seu email:                            |
+                |                                              |
+                ------------------------------------------------
+                """);
+        String emailUsuario = leitor.next();
+
+        Boolean existeEmail = validacaoEmail.verificarParametro(emailUsuario);
+
+
+        if (existeEmail.equals(true)) {
+            MaquinaDao maquinaDao = new MaquinaDao();
+            MaquinaTipoComponente maquinaTipoComponente = new MaquinaTipoComponente();
+            Timer timer = new Timer();
+
+            TimerTask inserirBanco = new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        maquinaDao.salvar();
+                        maquinaDao.salvarAWS();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            };
+            timer.scheduleAtFixedRate(inserirBanco, 0, 4000);
+        } else{
+            System.out.println("Não foi encontrado seu email. Contrate o nosso serviço primeiro. Obrigado");
+            Path path = Paths.get("/home/ubuntu/logs");
+            Path path1 = Paths.get("/home/ubuntu/logs/" + LocalDate.now());
+            File log = new File("/home/ubuntu/logs/" + LocalDate.now() + "/" + LocalDate.now() + ".txt");
+            LocalDateTime momentoAtual = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            String DateTimeFormatado = momentoAtual.format(formatter);
+
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+                if (!Files.exists(path1)){
+                Files.createDirectory(path1);
+                log.createNewFile();
+                FileWriter fw = new FileWriter(log, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                bw.write(DateTimeFormatado + " Tentativa de EMAIL incorreta...");
+                bw.newLine();
+
+                bw.close();
+                fw.close();}
+                else {
+                    FileWriter fw = new FileWriter(log, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+
+                    bw.write(DateTimeFormatado + " Tentativa de EMAIL incorreta...");
+                    bw.newLine();
+
+                    bw.close();
+                    fw.close();
+                }
+        }else {
+                if (Files.exists(path1)){
+                    log.createNewFile();
+                FileWriter fw = new FileWriter(log, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                bw.write(DateTimeFormatado + " Tentativa de EMAIL incorreta...");
+                bw.newLine();
+
+                bw.close();
+                fw.close();}else {
+                    Files.createDirectory(path1);
+                    log.createNewFile();
+                    FileWriter fw = new FileWriter(log, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+
+                    bw.write(DateTimeFormatado + " Tentativa de EMAIL incorreta...");
+                    bw.newLine();
+
+                    bw.close();
+                    fw.close();
+                }
+            }
+    }
+}
+}
